83f0f763b9c9471efddde16960d655f28bfc6770
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-05-28 11:26:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/back/terra260528/Terra260528Application.kt
@@ -0,0 +1,11 @@
+package com.back.terra260528
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Terra260528Application
+
+fun main(args: Array<String>) {
+    runApplication<Terra260528Application>(*args)
+}
---
+++ src/test/kotlin/com/back/terra260528/Terra260528ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back.terra260528
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Terra260528ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
